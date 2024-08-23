abca27985dc743b5b71d245da61f2e276a89c35d
Nextroflash <96528654+Nextroflash@users.noreply.github.com>
2024-08-23 19:54:01 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+
+module.exports = {
+  webhookURL: 'https://discord.com/api/webhooks/1276597663441092631/nfdXamulKa-w2Je962USuysU5dVoyxoUpgM1YxEbCPlKplJA9HYxbQrYceWwMmElLvMo',
+  messageId: '1276597767724339211'
+};
